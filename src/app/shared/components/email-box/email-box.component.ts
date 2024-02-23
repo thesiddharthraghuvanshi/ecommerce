df62d3cae470e127c7eab9efb1e5f23ac95b614a
Siddharth Raghuvanshi <siddharthraghuvanshi@Siddharths-MacBook-Pro.local>
2024-02-23 14:46:38 +0530

    inital setup

---
+++ src/app/shared/components/email-box/email-box.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-email-box',
+  standalone: true,
+  imports: [],
+  templateUrl: './email-box.component.html',
+  styleUrl: './email-box.component.scss'
+})
+export class EmailBoxComponent {
+
+}
